bbb94b66d632c802fef2cddeb7b6934e01c8a55f
Colin Murphy <colinmurphy1993@gmail.com>
2014-08-12 17:04:41 -0400

    Initial commit

---
+++ lua/autorun/NoFallDamage.lua
@@ -0,0 +1,15 @@
+
+
+
+-- The GetFallDamage hook does not get called until around 600 speed, which is a
+-- rather high drop already. Hence we do our own fall damage handling in
+-- OnPlayerHitGround.
+function GAMEMODE:GetFallDamage(ply, speed)
+   return 0
+end
+
+function GAMEMODE:OnPlayerHitGround(ply, in_water, on_floater, speed)
+   --Dont do anything
+end
+
+
