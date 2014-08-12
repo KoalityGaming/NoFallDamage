c162d5fc8b4d0ebe01e884e05e9a96904572aedc
Colin Murphy <colinmurphy1993@gmail.com>
2014-08-12 18:46:37 -0400

    Moved the overwrite to a startup hook.

--- lua/autorun/NoFallDamage.lua
+++ lua/autorun/NoFallDamage.lua
@@ -1,15 +1,14 @@
 
+hook.Add( "Initialize", "NoFallDamage_Override", function()
 
+	-- The GetFallDamage hook does not get called until around 600 speed, which is a
+	-- rather high drop already. Hence we do our own fall damage handling in
+	-- OnPlayerHitGround.
+	function GAMEMODE:GetFallDamage(ply, speed)
+	   return 0
+	end
 
--- The GetFallDamage hook does not get called until around 600 speed, which is a
--- rather high drop already. Hence we do our own fall damage handling in
--- OnPlayerHitGround.
-function GAMEMODE:GetFallDamage(ply, speed)
-   return 0
-end
-
-function GAMEMODE:OnPlayerHitGround(ply, in_water, on_floater, speed)
-   --Dont do anything
-end
-
-
+	function GAMEMODE:OnPlayerHitGround(ply, in_water, on_floater, speed)
+	   --Dont do anything
+	end
+end)
